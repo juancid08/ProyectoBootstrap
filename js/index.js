7a5b1d9ec7ef51d6d26effe0434e4e45180ba0fe
Juan Cid Benítez <juan.pizarra.19@gmail.com>
2024-02-17 12:51:02 +0100

    ...

--- js/index.js
+++ js/index.js
@@ -1,9 +1,37 @@
-/***************BOTON LEER MÁS******************/
-    $(document).ready(function() {
-        $('.read-more').click(function() {
-            $(this).siblings('.additional-text').slideToggle();
-            $(this).text(function(i, text) {
-                return text === "Read more" ? "Read less" : "Read more";
-            });
-        });
-    });
+/***************validadción******************/
+function validacion() {
+    let correcto = true;
+    let nombre = document.getElementById('nombre').value.trim();
+    let apellidos = document.getElementById('apellidos').value.trim();
+    let email = document.getElementById('email').value.trim();
+  
+    if (nombre === "" || /^\s+$/.test(nombre) || /[0-9]/.test(nombre) || !/[a-zñ]{2,}/i.test(nombre)) {
+      document.getElementById('nombreHelp').style.visibility = "visible";
+      document.getElementById('nombre').style.borderColor = "red";
+      correcto = false;
+    } else {
+      document.getElementById('nombreHelp').style.visibility = "hidden";
+      document.getElementById('nombre').style.borderColor = "";
+    }
+  
+    if (apellidos === "" || /^\s+$/.test(apellidos) || /[0-9]/.test(apellidos) || !/[a-zñ]{2,}/i.test(apellidos)) {
+      document.getElementById('apellidosHelp').style.visibility = "visible";
+      document.getElementById('apellidos').style.borderColor = "red";
+      correcto = false;
+    } else {
+      document.getElementById('apellidosHelp').style.visibility = "hidden";
+      document.getElementById('apellidos').style.borderColor = "";
+    }
+  
+    if (email !== "" && !/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,4})$/.test(email)) {
+      document.getElementById('emailHelp').style.visibility = "visible";
+      document.getElementById('email').style.borderColor = "red";
+      correcto = false;
+    } else {
+      document.getElementById('emailHelp').style.visibility = "hidden";
+      document.getElementById('email').style.borderColor = "";
+    }
+  
+    return correcto;
+  }
+  
