9ed5ea85adcd3c5572007d44966a8ba39cae41a5
Juan Cid Benítez <juan.pizarra.19@gmail.com>
2024-03-05 15:58:02 +0100

    ...

--- js/index.js
+++ js/index.js
@@ -1,37 +1,64 @@
-/***************validadción******************/
 function validacion() {
-    let correcto = true;
-    let nombre = document.getElementById('nombre').value.trim();
-    let apellidos = document.getElementById('apellidos').value.trim();
-    let email = document.getElementById('email').value.trim();
-  
-    if (nombre === "" || /^\s+$/.test(nombre) || /[0-9]/.test(nombre) || !/[a-zñ]{2,}/i.test(nombre)) {
-      document.getElementById('nombreHelp').style.visibility = "visible";
-      document.getElementById('nombre').style.borderColor = "red";
-      correcto = false;
-    } else {
-      document.getElementById('nombreHelp').style.visibility = "hidden";
-      document.getElementById('nombre').style.borderColor = "";
-    }
-  
-    if (apellidos === "" || /^\s+$/.test(apellidos) || /[0-9]/.test(apellidos) || !/[a-zñ]{2,}/i.test(apellidos)) {
-      document.getElementById('apellidosHelp').style.visibility = "visible";
-      document.getElementById('apellidos').style.borderColor = "red";
-      correcto = false;
-    } else {
-      document.getElementById('apellidosHelp').style.visibility = "hidden";
-      document.getElementById('apellidos').style.borderColor = "";
-    }
-  
-    if (email !== "" && !/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,4})$/.test(email)) {
-      document.getElementById('emailHelp').style.visibility = "visible";
-      document.getElementById('email').style.borderColor = "red";
-      correcto = false;
-    } else {
-      document.getElementById('emailHelp').style.visibility = "hidden";
-      document.getElementById('email').style.borderColor = "";
-    }
-  
-    return correcto;
+  let correcto = true;
+  let nombre = document.getElementById("nombre").value.trim();
+  let apellidos = document.getElementById("apellidos").value.trim();
+  let email = document.getElementById("email").value.trim();
+  let mensaje = document.getElementById("mensaje").value.trim();
+
+  if (
+    nombre === "" ||
+    /^\s+$/.test(nombre) ||
+    /[0-9]/.test(nombre) ||
+    !/[a-zñ]{2,}/i.test(nombre)
+  ) {
+    document.getElementById("nombreHelp").style.visibility = "visible";
+    document.getElementById("nombre").style.borderColor = "red";
+    correcto = false;
+  } else {
+    document.getElementById("nombreHelp").style.visibility = "hidden";
+    document.getElementById("nombre").style.borderColor = "";
   }
-  
+
+  if (
+    apellidos === "" ||
+    /^\s+$/.test(apellidos) ||
+    /[0-9]/.test(apellidos) ||
+    !/[a-zñ]{2,}/i.test(apellidos)
+  ) {
+    document.getElementById("apellidosHelp").style.visibility = "visible";
+    document.getElementById("apellidos").style.borderColor = "red";
+    correcto = false;
+  } else {
+    document.getElementById("apellidosHelp").style.visibility = "hidden";
+    document.getElementById("apellidos").style.borderColor = "";
+  }
+
+  if (
+    email === "" ||
+    !/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,4})$/.test(
+      email
+    )
+  ) {
+    document.getElementById("emailHelp").style.visibility = "visible";
+    document.getElementById("email").style.borderColor = "red";
+    correcto = false;
+  } else {
+    document.getElementById("emailHelp").style.visibility = "hidden";
+    document.getElementById("email").style.borderColor = "";
+  }
+
+  if (mensaje === "") {
+    document.getElementById("mensajeHelp").style.visibility = "visible";
+    document.getElementById("mensaje").style.borderColor = "red";
+    correcto = false;
+  } else {
+    document.getElementById("mensajeHelp").style.visibility = "hidden";
+    document.getElementById("mensaje").style.borderColor = "";
+  }
+
+  return correcto;
+}
+function resetear(id) {
+  document.getElementById(id + "Help").style.visibility = "hidden";
+  document.getElementById(id).style.borderColor = "lightgray";
+}
